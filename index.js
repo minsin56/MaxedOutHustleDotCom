027b277980669083c138313ab4f9602f805c2d31
minsin56 <42984786+minsin56@users.noreply.github.com>
2020-10-13 19:06:19 -0500

    big

---
+++ index.js
@@ -0,0 +1,41 @@
+const NodeMailer = require("nodemailer");
+const http = require("http");
+
+const Server = http.createServer((Req,Resp) => 
+{
+    const {method,url} = Req;
+    const {headers} = Req;
+    const To = headers['to'];
+    const Messge = headers['message'];
+
+    SendMail().catch(console.error);
+});
+
+async function SendMail()
+{
+    
+    let Transporter = nodemailer.createTransport(
+        {
+            host: 'smtp.hostinger.com',
+            port: 587,
+            secure: false,
+            auth: 
+            {
+                user: 'contact@maxedouthustle.com',
+                pass: 'Jackson1'
+            }
+        });
+    
+    let Info = await Transporter.sendMail(
+        {
+            from: 'contact@maxedouthustle.com',
+            to: "castrovincent985@gmail.com",
+            subject: "hello",
+            text: "hello world?",
+            html: "<b>hello world?</b>"
+        });
+
+    console.log(Info.messageId);
+}
+
+Server.listen(8080);
